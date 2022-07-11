df8dd9be99371fd61d58012ca60ca625418831ce
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-11 12:03:06 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int input;
+    printf("Enter a number ");
+    scanf("%d",&input);
+    if(input%2==0)
+    {
+        printf("number is even");
+    }
+    else 
+    {
+        printf("number is odd");
+    }
+}
