318a10909d8937cbd1688b7bf886a19bb2330c77
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-11 12:52:47 +0530

    Create Q6.c

---
+++ Q6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int inp1,inp2;
+  printf("Enter the first number");
+  scanf("%d",&inp1);
+  printf("Enter the second number");
+  scanf("%d",&inp2);
+  if(inp1<inp2)
+  {
+      printf("%d",inp2);
+  }
+  else 
+  {
+      printf("%d",inp1);
+  }
+  
+  
+  
+}
