a5531550a692bd1ed398afd0db8cb2c1f6266166
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-13 14:59:49 +0530

    Create Q11.c

---
+++ Q11.c
@@ -0,0 +1,60 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    double sub1,sub2,sub3,sub4,sub5;
+    printf("Enter marks\n");
+    scanf("%lf",&sub1);
+    if(sub1>33 && sub1<=100)
+    {
+        printf("Passed");
+    }
+    else
+    {
+        printf("failed");
+    }
+    printf("Enter marks\n");
+    scanf("%lf",&sub2);
+    if(sub2>33 && sub2<=100)
+    {
+        printf("Passed");
+    }
+    else
+    {
+        printf("failed");
+    }
+    printf("Enter marks\n");
+    scanf("%lf",&sub3);
+    if(sub3>33 && sub3<=100)
+    {
+        printf("Passed");
+    }
+    else
+    {
+        printf("failed");
+    }
+    printf("Enter marks\n");
+    scanf("%lf",&sub4);
+    if(sub4>33 && sub4<=100)
+    {
+        printf("Passed");
+    }
+    else
+    {
+        printf("failed");
+    }
+    printf("Enter marks\n");
+    scanf("%lf",&sub5);
+    if(sub5>33 && sub5<=100)
+    {
+        printf("Passed");
+    }
+    else
+    {
+        printf("failed");
+    }
+    
+
+        
+    return 0;
+}
